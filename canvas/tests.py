bf71b5963db6be6dce1bda7f117cb153ec216919
abecode <abe.kazemzadeh@gmail.com>
2022-03-12 16:09:00 -0600

    starting to get tests for sync

--- canvas/tests.py
+++ canvas/tests.py
@@ -1,3 +1,20 @@
+import json
 from django.test import TestCase
 
 # Create your tests here.
+class RestJsonTest(TestCase):
+    def setup(self):
+        self.log =  logging.getLogger("RestJsonTest")
+
+    def test_read_json(self):
+        records_before = {}
+        records_after = {}
+        with open("canvas/test_data/courses_before.json") as f:
+            jsondata = ''.join(line for line in f if not line.startswith('//'))
+            records_before = json.loads(jsondata)
+        with open("canvas/test_data/courses_after.json") as f:
+            jsondata = ''.join(line for line in f if not line.startswith('//'))
+            records_after = json.loads(jsondata)
+        
+        self.assertEqual(len(records_before), 30)
+        self.assertEqual(len(records_after), 33)
